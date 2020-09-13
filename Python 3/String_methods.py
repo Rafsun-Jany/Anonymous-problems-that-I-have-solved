25f5a31b9a55acf3f5169e267b0f211f81b8b31c
Mohammad Rafsun Jany Mahin <59613041+Rafsun-Jany@users.noreply.github.com>
2020-09-14 00:02:20 +0600

    Add files via upload

---
+++ Python 3/String_methods.py
@@ -0,0 +1,91 @@
+# I have discussed total 22 string methods of python
+
+a = 'Bangladesh'
+
+print(a.capitalize())
+
+a = 'Bangladesh'
+
+print(a.count('a'))
+
+a = 'Bangladesh'
+
+print(a.index('g'))
+
+a = 'Bangladesh-is-our-motherland'
+
+print(a.split("-"))
+
+a = 'Bangladesh is our motherland'
+
+print(a.title())
+
+a = 'Bangladesh'
+
+print(a.casefold())
+
+a = 'Bangladesh'
+
+print(a.endswith('p'))
+
+a = 'Bangladesh'
+
+print(a.isalnum())
+
+a = 'Bangladesh'
+
+print(a.isalpha())
+
+a = 'Bangladesh'
+
+print(a.isdigit())
+
+a = 'Bangladesh'
+
+print(a.startswith('k'))
+
+a = 'Bangladesh'
+
+print(a.find('desh'))
+
+a = 'Bangladesh'
+
+print(a.islower())
+
+a = 'Bangladesh'
+
+print(a.isupper())
+
+a = 'Bangladesh'
+
+print(a.isnumeric())
+
+a = 'Bangladesh'
+
+print(a.lower())
+
+a = 'Bangladesh'
+
+print(a.upper())
+
+a = 'Bangladesh'
+
+print(a.rjust(40))
+
+a = '    Bangladesh'
+
+print(a.strip())
+
+a = 'Bangladesh'
+
+print(a.swapcase())
+
+a = 'Bangladesh'
+
+print(a.isprintable())
+
+a = 'Bangladesh'
+
+print(a.join(a))
+
+
